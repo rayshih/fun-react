d87e95933076b0482284dc4d77497d762abc7487
Ray Shih <mnf.shih@gmail.com>
2016-07-17 01:49:17 +0800

    implement link and map event functions

--- app/App.js
+++ app/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import {component} from './fun'
 
-export default component('App', (event, props) => {
+const Counter = component('Counter', (event, props) => {
   return props.get('store').map(store => (
     <div>
       <h1>Counter: {store}</h1>
       <button onClick={event('inc')}>INC</button>
       <button onClick={event('dec')}>DEC</button>
     </div>
+  ))
+})
+
+export default component('App', (event, props) => {
+  const {link} = event
+  return props.get('store').map(store => (
+    link(<Counter store={store} />)
   ))
 })
--- app/Fun.js
+++ app/Fun.js
@@ -1,27 +1,43 @@
+import React from 'react'
 import Cycle from 'cycle-react'
 import {Subject} from 'rx'
+
+// util functions
+const id = a => a
 
 export const component = (componentName, defFn, componentOptions) => {
   const cycleDefFn = (interaction, props, self, lifecycles) => {
     // event system
     const eventTypes = []
     const event$ = new Subject()
+    const sendEvent = event => event$.onNext(event)
 
     // register function
-    const event = eventType => {
+    const registerEvent = eventType => {
       if (!eventTypes.includes(eventType)) {
         eventTypes.push(eventType)
 
         interaction.get(eventType).subscribe(payload => {
-          event$.onNext({eventType, payload})
+          sendEvent({eventType, payload})
         })
       }
 
       return interaction.listener(eventType)
     }
 
+    const mapEvent = mapper => element => {
+      return React.cloneElement(element, {
+        onEvent: event => sendEvent(mapper(event))
+      })
+    }
+
+    const linkEvent = mapEvent(id)
+
+    registerEvent.map = mapEvent
+    registerEvent.link = linkEvent
+
     // only allow view
-    const view = defFn(event, props, self, lifecycles)
+    const view = defFn(registerEvent, props, self, lifecycles)
 
     return {
       view,
