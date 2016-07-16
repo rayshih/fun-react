29c4a1b0a852a1d9a67aa2f4f337933e06abb7f4
Ray Shih <mnf.shih@gmail.com>
2016-07-17 00:40:42 +0800

    the simple counter by cycle react

--- app/App.js
+++ app/App.js
@@ -2,6 +2,20 @@ import React from 'react'
 import {component} from 'cycle-react'
 import {Observable} from 'rx'
 
-export default component('App', () => {
-  return Observable.just(<h1>Hello world</h1>)
+export default component('App', (interaction, props) => {
+  const view = props.get('store').map(store => (
+    <div>
+      <h1>Counter: {store}</h1>
+      <button onClick={interaction.listener('inc')}>INC</button>
+      <button onClick={interaction.listener('dec')}>DEC</button>
+    </div>
+  ))
+
+  return {
+    view,
+    events: {
+      onInc: interaction.get('inc'),
+      onDec: interaction.get('dec')
+    }
+  }
 })
--- app/index.js
+++ app/index.js
@@ -1,5 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import {Observable, Subject} from 'rx'
+import Immutable from 'immutable'
+
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const inc$ = new Subject()
+const inc = () => inc$.onNext()
+
+const dec$ = new Subject()
+const dec = () => dec$.onNext()
+
+const store = Observable.merge(
+  inc$.map(() => model => model + 1),
+  dec$.map(() => model => model - 1)
+)
+.scan((store, mod) => mod(store), 0)
+.startWith(0)
+
+// main
+const rootEl = document.getElementById('app')
+store.subscribe(store => {
+  ReactDOM.render(
+    <App
+      store={store}
+      onInc={inc}
+      onDec={dec}
+    />, rootEl)
+})
