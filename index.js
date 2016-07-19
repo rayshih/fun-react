b058ed64aabfde733c6f624216b8b50d193578c3
Ray Shih <mnf.shih@gmail.com>
2016-07-20 02:22:04 +0800

    make mapEvent become a real map

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Cycle from 'cycle-react'
-import _component from './lib/component'
 
+import {
+  mapEvent as _mapEvent,
+  component as _component
+} from './lib/component'
+
+export const mapEvent = _mapEvent(React, Cycle)
 export const component = _component(React, Cycle)
+
 
 export * from './lib/architecture'
 export * from './lib/html'
