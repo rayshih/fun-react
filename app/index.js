f4e70390f6a43c7c72a2ac3da84e43340c0c0444
Ray Shih <mnf.shih@gmail.com>
2016-07-20 01:14:03 +0800

    implement nested examples

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
 /* global document */
 
-import {beginnerProgram} from '../index'
-import App, * as AM from './App'
+// import {beginnerProgram} from '../index'
+// import App, * as AM from './App'
+
+import React from 'react'
+import ReactDom from 'react-dom'
+
+// import CycleNested from './CycleNested'
+import FunNested from './FunNested'
 
 const rootEl = document.getElementById('app')
-beginnerProgram({
-  model: AM.init(0, 0, 0),
-  update: AM.update,
-  view: App
-}, rootEl)
 
+// ReactDom.render(<CycleNested />, rootEl)
+ReactDom.render(<FunNested />, rootEl)
+
+// beginnerProgram({
+//   model: AM.init(0, 0, 0),
+//   update: AM.update,
+//   view: App
+// }, rootEl)
