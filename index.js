a66d816e6f707bcd2867e1497357fff0eff20fb0
Ray Shih <mnf.shih@gmail.com>
2016-07-21 10:14:29 +0800

    restructure folders as a library

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-import React from 'react'
-import Cycle from 'cycle-react'
-
-import {
-  mapEvent as _mapEvent,
-  component as _component
-} from './lib/component'
-
-export const mapEvent = _mapEvent(React, Cycle)
-export const component = _component(React, Cycle)
-
-export * from './lib/architecture'
-export * from './lib/html'
+module.exports = require('./lib/index')
--- native.js
+++ native.js
@@ -1,12 +1,1 @@
-import React from 'react-native'
-import Cycle from 'cycle-react/native'
-
-import {
-  mapEvent as _mapEvent,
-  component as _component
-} from './lib/component'
-
-export const mapEvent = _mapEvent(React, Cycle)
-export const component = _component(React, Cycle)
-
-export * from './lib/architecture'
+module.exports = require('./lib/native')
