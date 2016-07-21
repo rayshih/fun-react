51d674d622c2cb0a0f712c344ce428106ae2ea75
Ray Shih <mnf.shih@gmail.com>
2016-07-21 10:42:44 +0800

    fix import again

--- src/native.js
+++ src/native.js
@@ -4,7 +4,7 @@ import Cycle from 'cycle-react/native'
 import {
   mapEvent as _mapEvent,
   component as _component
-} from './lib/component'
+} from './component'
 
 export const mapEvent = _mapEvent(React, Cycle)
 export const component = _component(React, Cycle)
