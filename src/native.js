0107bf4d17b76f4f4fcaa0298424402ef5c1e7e6
Ray Shih <mnf.shih@gmail.com>
2016-08-23 23:01:35 +0800

    simplify native.js

--- src/native.js
+++ src/native.js
@@ -1,14 +1,2 @@
-import React from 'react'
-import Cycle from 'cycle-react'
-
-import {
-  mapEvent as _mapEvent,
-  component as _component
-} from './component'
-
-export * from './type-system'
-
-export const mapEvent = _mapEvent(React, Cycle)
-export const component = _component(React, Cycle)
-
-export * from './architecture'
+// TODO remove this file later
+export * from './index'
