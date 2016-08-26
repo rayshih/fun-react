8446bbc58593fa26989b3d0dfcf3eaf7f1a24a4d
Ray Shih <mnf.shih@gmail.com>
2016-08-27 01:11:36 +0800

    add flowtype and pass it

--- src/architecture.js
+++ src/architecture.js
@@ -1,11 +1,11 @@
 import {caseOf} from './type-system'
 
-export const createUpdate = updateMap => {
+export const createUpdate = (updateMap) => {
   const fnMap = {...updateMap}
 
   if (fnMap._otherwise) {
     fnMap._otherwise = (_, model) => model
   }
 
-  return caseOf(updateMap)
+  return caseOf(fnMap)
 }
--- src/type-system.js
+++ src/type-system.js
@@ -1,4 +1,10 @@
-export const createTypes = (...typeNames) => (
+// @flow
+
+type Typed<P> = { type: string, payload: P }
+type TypedCtor<P> = (payload: P) => Typed<P>
+type TypedCtorMap = { [key: string]: TypedCtor<any> }
+
+export const createTypes = (...typeNames: Array<string>): TypedCtorMap => (
   typeNames.reduce((r, n) => {
     const ctor = payload => ({
       type: n,
@@ -13,7 +19,10 @@ export const createTypes = (...typeNames) => (
   }, {})
 )
 
-export const caseOf = fnMap => (obj, ...rest) => {
+type MapFn<P, R> = (obj: Typed<P>, rest: Array<any>) => R
+type FnMap = { [key: string]: MapFn<any, any> }
+
+export const caseOf = (fnMap: FnMap) => (obj: Typed<any>, ...rest: Array<any>) => {
   const otherwise = fnMap._otherwise
   const fn = fnMap[obj.type]
 
@@ -28,7 +37,9 @@ export const caseOf = fnMap => (obj, ...rest) => {
   return obj // by pass if not handled
 }
 
-export const compose = (f, g) => {
+type Fn<A, B> = (a: A) => B
+
+export const compose = <A, B, C> (f: Fn<B, C>, g: Fn<A, B>): Fn<A, C> => {
   const h = x => f(g(x))
   h.typeName = f.typeName
   return h
